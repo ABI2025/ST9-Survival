2d732d72a59f4f932dee012ad8ac5464e727b068
KaempferNr1 <Jonathan@ebener.net>
2024-01-18 22:11:04 +0100

    gui klasse und funktionen hinzugefügt

---
+++ st9/src/Gui.cpp
@@ -0,0 +1,36 @@
+#include "Gui.h"
+
+#include "imgui-SFML.h"
+#include "imgui.h"
+#include "../Resources/Roboto-regular.embed"
+
+bool init_sfml_imgui(sf::RenderWindow& window)
+{
+	if (!ImGui::SFML::Init(window)) return false;
+
+    ImGuiIO& io = ImGui::GetIO();
+    io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;       // Enable Keyboard Controls
+    io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;           // Enable Docking
+    io.ConfigFlags |= ImGuiConfigFlags_ViewportsEnable;         // Enable Multi-Viewport / Platform Windows
+
+    ImFontConfig fontConfig;
+    fontConfig.FontDataOwnedByAtlas = false;
+    ImFont* robotoFont = io.Fonts->AddFontFromMemoryTTF((void*)g_RobotoRegular, sizeof(g_RobotoRegular), 20.0f, &fontConfig);
+    io.FontDefault = robotoFont;
+    ImGui::SFML::UpdateFontTexture();
+
+    // Setup Dear ImGui style
+    ImGui::StyleColorsDark();
+
+    // When viewports are enabled we tweak WindowRounding/WindowBg so platform windows can look identical to regular ones.
+    ImGuiStyle& style = ImGui::GetStyle();
+    if (io.ConfigFlags & ImGuiConfigFlags_ViewportsEnable)
+    {
+        style.WindowRounding = 0.0f;
+        style.Colors[ImGuiCol_WindowBg].w = 1.0f;
+    }
+}
+
+Gui::Gui(std::initializer_list<GuiElement> gui_elements) : m_elements(gui_elements)
+{ 
+}
---
+++ st9/src/Gui.h
@@ -0,0 +1,21 @@
+#pragma once
+#include <vector>
+#include <SFML/Graphics/RenderWindow.hpp>
+//returns false on fail
+bool init_sfml_imgui(sf::RenderWindow& window);
+
+
+
+class GuiElement
+{
+	
+};
+
+
+class Gui
+{
+	std::vector<GuiElement> m_elements;
+public:
+	Gui(std::initializer_list<GuiElement>);
+};
+
