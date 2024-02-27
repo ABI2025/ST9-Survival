bd8ee72530142dcbe46c518630c5bbb9a4caf85b
maxho <maxho@LAPTOP-R0EKIH8H>
2024-02-27 14:50:09 +0100

    speed und draw von Projektil angepasst.

--- st9/src/projektil.cpp
+++ st9/src/projektil.cpp
@@ -6,7 +6,7 @@
 #include <cmath>
 #include "Enemy.h"
 
-projectil::projectil(int speed, sf::Vector2f pos) {
+projectil::projectil(sf::Vector2f speed, sf::Vector2f pos) {
 	m_speed = speed;
 	m_pos = pos;
 }
@@ -15,22 +15,23 @@ projectil::~projectil() {
 
 }
 
-void projectil::setSpeed(int speed) {
+void projectil::set_Speed(sf::Vector2f speed) {
 	m_speed = speed;
 }
-void projectil::setVec2(sf::Vector2f pos) {
+void projectil::set_Pos(sf::Vector2f pos) {
 	m_pos = pos;
 }
-int projectil::getSpeed() {
+sf::Vector2f projectil::get_Speed() {
 	return m_speed;
 }
-sf::Vector2f projectil::getVec2() {
+sf::Vector2f projectil::get_Pos() {
 	return m_pos;
 }
 void projectil::drawProjectil(sf::RenderTarget& window, Enemy& m_enemy ) {
 
 	sf::RectangleShape projectile(sf::Vector2f(10,20));
 	projectile.setFillColor(sf::Color::Red);
+	m_pos += m_speed;
 	projectile.setPosition(m_pos);
 	projectile.setRotation((-1)*(atan2( projectile.getPosition().x - m_enemy.get_pos().x, projectile.getPosition().y - m_enemy.get_pos().y) * 180 / 3.14159265358979311600 )); //Soll in Richtung Ziel zeigen
 
