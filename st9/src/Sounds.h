aeb2033c23c4eecaa0fa7c90b0b86cb374712712
KaempferNr1 <Jonathan@ebener.net>
2024-05-07 15:09:35 +0200

    sound system verbessert

--- st9/src/Sounds.h
+++ st9/src/Sounds.h
@@ -1,33 +1,38 @@
 #pragma once
 #include <deque>
 #include <map>
-#include <ranges>
 #include <vector>
 #include <SFML/Audio.hpp>
 
 class Sounds
 {
-	std::vector<sf::SoundBuffer> m_buffers;
-	std::vector<std::pair<std::deque<sf::Sound>,bool>> m_sounds;
-	std::map<int,float> volumes;
+	std::map<std::string,std::vector<sf::SoundBuffer>> m_buffers;
+	std::map<std::string,std::vector<std::pair<std::deque<sf::Sound>,bool>>> m_sounds;
+
+
+	std::map<int,float> m_volumes;
+	std::vector<std::string> m_;
 public:
-	Sounds()
-	{
-		volumes[-1] = 1.0f;
-	}
+	Sounds();
 	~Sounds();
 	//fügt einen sound hinzu und spielt ihn ab
-	void add_sound(int id);
+	void add_sound(int group_id,int id);
+
+	//fügt einen sound hinzu und spielt ihn ab
+	void add_sound(const std::string& group_id,int id);
 
 	//Löscht alle nicht spielenden Sounds
 	void cleanup(bool priority_ignorieren);
 
 	//Lädt eine music file spezifiziert in location und erstellt eine neue deque für sounds
-	void load_buffer(const std::string& location, bool priority);
+	void load_buffer(const std::string& location, bool priority, const std::string& group);
+
+	//
+	void add_group(const std::string& group);
 
 	//pausiert alle nicht gestoppten lieder
 	void pause_all(bool priority_ignorieren);
-
+		
 	//spielt alle sounds ab die nicht gerade abgespielt werden
 	void play_all();
 
